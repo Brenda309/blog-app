b277b344dc3db30e225eb965bce5aac5ae8ad761
Brenda Wihogora <brewihogora23@gmail.com>
2022-09-22 01:05:39 +0200

    Add database

---
+++ db/migrate/20220921222337_rename_user_id_to_name_in_author_id.rb
@@ -0,0 +1,4 @@
+class RenameUserIdToNameInAuthorId < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
---
+++ db/migrate/20220921225146_renameuser_id_toauthor_id.rb
@@ -0,0 +1,4 @@
+class RenameuserIdToauthorId < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
