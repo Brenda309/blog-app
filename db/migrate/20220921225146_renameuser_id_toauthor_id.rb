2ee7bba64a29eefbaca1ddaa13bcded950a2885c
Brenda Wihogora <brewihogora23@gmail.com>
2022-09-22 11:44:49 +0200

    Rename the column user_id to author_id

--- db/migrate/20220921225146_renameuser_id_toauthor_id.rb
+++
@@ -1,4 +0,0 @@
-class RenameuserIdToauthorId < ActiveRecord::Migration[7.0]
-  def change
-  end
-end
--- db/migrate/20220921225733_rename_user_id_to_author_id.rb
+++ db/migrate/20220921225733_rename_user_id_to_author_id.rb
@@ -1,4 +1,13 @@
 class RenameUserIdToAuthorId < ActiveRecord::Migration[7.0]
-  def change
+  def up
+    rename_column :posts, :user_id, :author_id
+    rename_column :likes, :user_id, :author_id
+    rename_column :comments, :user_id, :author_id
+  end
+
+  def down
+    rename_column :posts, :user_id, :author_id
+    rename_column :likes, :user_id, :author_id
+    rename_column :comments, :user_id, :author_id
   end
 end
