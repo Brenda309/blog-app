5021fd65ea84cbcc9cfeeb361bacf536cc8518ad
Brenda Wihogora <brewihogora23@gmail.com>
2022-09-23 23:45:41 +0200

    Add the models

--- db/migrate/20220923213251_rename_user_id_to_author_id.rb
+++ db/migrate/20220923213251_rename_user_id_to_author_id.rb
@@ -1,12 +1,7 @@
 class RenameUserIdToAuthorId < ActiveRecord::Migration[7.0]
-  def self.up
+  def change
     rename_column :posts, :user_id, :author_id
-    rename_column :comments, :user_id, :author_id
     rename_column :likes, :user_id, :author_id
+    rename_column :comments, :user_id, :author_id
   end
-  def down
-    rename_column :posts, :user_id, :author_id
-    rename_column :comments, :user_id, :author_id
-    rename_column :likes, :user_id, :author_id
-  end 
 end
---
+++ test/models/add_post_ref_to_comment_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class AddPostRefToCommentTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
