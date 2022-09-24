ffe4b1ade1f7085d05db8560140f0fc84805f53c
Brenda Wihogora <brewihogora23@gmail.com>
2022-09-24 07:01:13 +0200

    Add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
 
+  validates :name, presence: true
+  validates :comments_counter, numericality: {greater_than_or_equal_to: 0}
+
   def last_three_posts
     Post.where(author: self).order(created_at: :desc).first(3)
   end
