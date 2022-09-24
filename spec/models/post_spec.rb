2b94eb2e29277d982fd012a3cb3ee04cddc01c0f
Brenda Wihogora <brewihogora23@gmail.com>
2022-09-24 09:10:31 +0200

    Fix linters

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,25 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  #tests go here
- subject { Post.create(title: 'My comment', author: User.new(name: 'Brenda')) }
+  # tests go here
+  subject { Post.create(title: 'My comment', author: User.new(name: 'Brenda')) }
 
-   it 'title should be present' do 
+  it 'title should be present' do
     subject.title = nil
     expect(subject).to_not be_valid
-   end
+  end
 
-   it 'title should not be over 250 words' do
+  it 'title should not be over 250 words' do
     subject.title = 250
     expect(subject).to_not be_valid
-   end
-   it 'comment counter should be greater than or equal to 0' do
-   subject.comments_counter = -1
-   expect(subject).to_not be_valid
-   end 
+  end
+  it 'comment counter should be greater than or equal to 0' do
+    subject.comments_counter = -1
+    expect(subject).to_not be_valid
+  end
 
-   it 'like counter should be greater than or equal to 0' do
-   subject.likes_counter = -1
-   expect(subject).to_not be_valid
-   end 
+  it 'like counter should be greater than or equal to 0' do
+    subject.likes_counter = -1
+    expect(subject).to_not be_valid
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  #tests go here
-  subject{User.new(name:'Brenda')}
+  # tests go here
+  subject { User.new(name: 'Brenda') }
 
-   it 'name should be present' do 
+  it 'name should be present' do
     subject.name = nil
     expect(subject).to_not be_valid
-   end
+  end
 
-   it 'post counter should be greater than or equal to 0' do
-   subject.post_counter = -1
-   expect(subject).to_not be_valid
-   end 
+  it 'post counter should be greater than or equal to 0' do
+    subject.post_counter = -1
+    expect(subject).to_not be_valid
+  end
 end
