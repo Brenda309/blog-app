a3f7666f9400ca2222f43e26732424643d0c2288
Brenda Wihogora <brewihogora23@gmail.com>
2022-09-29 18:12:42 +0200

    Add the request controller test for posts

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  before(:each) do
+    get '/users/:id/posts'
+  end
+  describe 'GET /users/:id/posts' do
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+    it 'If a correct template was rendered.' do
+      expect(response).to render_template(:index)
+    end
+    it 'If the response body includes correct placeholder text.' do
+      expect(response.body).to include('Posts')
+    end
+  end
+
+  describe 'GET /users/:id/posts/show' do
+    before(:each) do
+    get '/users/:id/posts/show'
+  end
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+    it 'If a correct template was rendered.' do
+      expect(response).to render_template(:show)
+    end
+    it 'If the response body includes correct placeholder text.' do
+      expect(response.body).to include('Post # by user name')
+    end
   end
 end
