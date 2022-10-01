73a086274c63b0e7f8675393ac63661412400eed
Brenda Wihogora <brewihogora23@gmail.com>
2022-10-01 10:04:24 +0200

    Add the user view

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,10 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
 
-  validates :name, presence: true
-  validates :post_counter, numericality: { greater_than_or_equal_to: 0 }
+  # validates :name, presence: true
+  # validates :post_counter, numericality: { greater_than_or_equal_to: 0 }
 
-  def last_three_posts
-    Post.where(author: self).order(created_at: :desc).first(3)
-  end
+  # def last_three_posts
+  #   Post.where(author: self).order(created_at: :desc).first(3)
+  # end
 end
