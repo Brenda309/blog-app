99ac3ad36a1c9d3be88e61777d59bc0d022b95b5
Brenda Wihogora <brewihogora23@gmail.com>
2022-10-02 01:37:02 +0200

    Add data to database

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,18 +1,18 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
+  belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
 
   after_save :update_user_counter
 
-  validates :title, presence: true, length: { maximum: 250 }
-  validates :comments_counter, :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  # validates :title, presence: true, length: { maximum: 250 }
+  # validates :comments_counter, :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def update_user_counter
-    author.update!(post_counter: author.posts.count)
+    author.update(post_counter: author.posts.count)
   end
 
-  def recent_5_comments
+  def five_most_ricent_comments
     comments.order(created_at: :desc).limit(5)
   end
 
