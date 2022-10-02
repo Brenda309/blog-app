56e5844effb94930c14eab56e1c44fa1cd493a8a
Brenda Wihogora <brewihogora23@gmail.com>
2022-10-02 02:38:24 +0200

    Add the routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-
-    resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+  devise_for :users
+  resources :users, only: [:index, :show] do
+    resources :posts do
+      resources :comments, only: [:create, :destroy]
+      resources :likes, only: [:create, :destroy]
     end
+  end
      root :to => "users#index"
 end
