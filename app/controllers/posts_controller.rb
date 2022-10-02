ab7e3888278e513b6b3e43c098eeb00090ae156e
Brenda Wihogora <brewihogora23@gmail.com>
2022-10-02 02:30:22 +0200

    Add view and controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,33 @@ class PostsController < ApplicationController
   end
 
   def show
+    @user = current_user
     @posts = Post.find(params[:id])
     @comments = @posts.comments
   end
+
+
+   def new
+    @user = current_user
+    @post = Post.new
+  end
+
+  def create
+    @user = current_user
+    @post = @user.posts.create(post_params)
+    if @post.save
+      flash[:success] = 'Post saved successfully'
+      redirect_to user_posts_path(@user.id)
+    else
+      flash.now[:error] = 'Error: Post could not be saved'
+      render :new, status: 422
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
+
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,8 +5,8 @@ class Post < ApplicationRecord
 
   after_save :update_user_counter
 
-  # validates :title, presence: true, length: { maximum: 250 }
-  # validates :comments_counter, :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def update_user_counter
     author.update(post_counter: author.posts.count)
